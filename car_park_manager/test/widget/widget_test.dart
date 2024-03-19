b7766a2e9b84d0c816d42a94fac170903c799802
gufdoor <gabriel.anjos.1280572@sga.pucminas.br>
2024-03-18 23:54:55 -0300

    FIX: fixing widget tests

--- car_park_manager/test/widget/widget_test.dart
+++ car_park_manager/test/widget/widget_test.dart
@@ -1,37 +1,61 @@
-import 'package:car_park_manager/main.dart';
+import 'package:car_park_manager/src/modules/management/domain/enums/parking_space_status_enum.dart';
+import 'package:car_park_manager/src/modules/management/domain/models/parking_space_model.dart';
+import 'package:car_park_manager/src/modules/management/domain/models/register_model.dart';
+import 'package:car_park_manager/src/modules/management/widgets/custom_parking_space_tile.dart';
+import 'package:car_park_manager/src/modules/management/widgets/custom_register_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group("FormFields", () {
-    testWidgets("When setting vacancies then is expected that the input text is saved in a controller", (tester) async {
-      // arrange
-      final vacanciesField = find.byKey(const ValueKey("vacancies"));
-      final vacanciesButton = find.byKey(const ValueKey("setVacancies"));
+  group("Tiles", () {
+    testWidgets(
+      "When CustomParkingSpaceTile is called then 'tis expected a tile will be build properly with the correct texts.",
+      (tester) async {
+        // arrange
+        final RegisterModel register = RegisterModel()
+          ..parkingSpaceId = 0
+          ..licensePlate = "abc1234";
+        final ParkingSpaceModel parkingSpace = ParkingSpaceModel(id: 0)
+          ..status = ParkingSpaceStatusEnum.vacant
+          ..registers = [register];
 
-      // act
-      await tester.pumpWidget(const MainApp());
-      await tester.enterText(vacanciesField, "10");
-      await tester.tap(vacanciesButton);
-      await tester.pump();
+        // act
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: CustomParkingSpaceTile(
+                parkingSpaceIndex: 0,
+                parkingSpace: parkingSpace,
+              ),
+            ),
+          ),
+        );
 
-      // assert
-      expect(find.text("10"), findsOneWidget);
-    });
+        // assert
+        expect(find.text("Vaga 0"), findsOneWidget);
+        expect(find.text("Placa: abc1234"), findsOneWidget);
+      },
+    );
 
-    testWidgets("When setting licensePlate then is expected that the input text is saved in a controller", (tester) async {
-      // arrange
-      final licensePlateField = find.byKey(const ValueKey("licensePlate"));
-      final licensePlateButton = find.byKey(const ValueKey("setLicensePlate"));
+    testWidgets(
+      "When CustomRegisterTile is called then 'tis expected a tile will be build properly with the correct texts.",
+      (tester) async {
+        // arrange
+        final RegisterModel register = RegisterModel()
+          ..checkOut = 946684800000
+          ..parkingSpaceId = 0;
 
-      // act
-      await tester.pumpWidget(MaterialApp(home: Container()));
-      await tester.enterText(licensePlateField, "abc1234");
-      await tester.tap(licensePlateButton);
-      await tester.pump();
+        // act
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(body: CustomRegisterTile(register: register)),
+          ),
+        );
 
-      // assert
-      expect(find.text("abc1234"), findsOneWidget);
-    });
+        // assert
+        expect(find.text("Vaga 0"), findsOneWidget);
+        expect(find.text("Check-out: 31/12/1999 22:00"), findsOneWidget);
+      },
+    );
   });
 }
